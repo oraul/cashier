3ea1d553383ebc6e38c5634cfd6f2276ee84c321
oraul <1838418+oraul@users.noreply.github.com>
2023-07-07 10:23:54 -0300

    Create product entity validation

--- lib/entities/product_entity.rb
+++ lib/entities/product_entity.rb
@@ -1,7 +1,29 @@
 # frozen_string_literal: true
 
 ProductEntity = Struct.new(:code, :name, :price_in_cents, :rule, keyword_init: true) do
+  attr_reader :errors
+
   def calculate_in_cents(total)
     rule.call(total, price_in_cents)
+  end
+
+  def valid?
+    @errors = Hash.new { |hash, key| hash[key] = [] }
+
+    @errors[:code] << 'is empty' if empty?(:code)
+    @errors[:name] << 'is empty' if empty?(:name)
+    @errors[:price_in_cents] << 'is not integer' unless integer?(:price_in_cents)
+
+    @errors.empty?
+  end
+
+  private
+
+  def empty?(attribute_name)
+    public_send(attribute_name).nil? || public_send(attribute_name).empty?
+  end
+
+  def integer?(attribute_name)
+    public_send(attribute_name).instance_of?(Integer)
   end
 end
--- spec/lib/entities/product_entity_spec.rb
+++ spec/lib/entities/product_entity_spec.rb
@@ -2,9 +2,7 @@
 
 RSpec.describe ProductEntity do
   subject(:product_entity) do
-    build(:product_entity, :coffee, rule: lambda { |total, price_in_cents|
-                                            total * price_in_cents
-                                          })
+    build(:product_entity, :coffee, rule: ->(total, price_in_cents) { total * price_in_cents })
   end
 
   describe 'attributes' do
@@ -18,6 +16,34 @@ RSpec.describe ProductEntity do
     end
 
     it { is_expected.to have_attributes(attributes) }
+  end
+
+  describe 'validations' do
+    before do
+      product_entity.public_send(:"#{attribute_name}=", nil)
+      product_entity.valid?
+    end
+
+    context 'when code is empty' do
+      let(:attribute_name) { :code }
+
+      it { is_expected.not_to be_valid }
+      it { expect(product_entity.errors).to match(code: ['is empty']) }
+    end
+
+    context 'when name is empty' do
+      let(:attribute_name) { :name }
+
+      it { is_expected.not_to be_valid }
+      it { expect(product_entity.errors).to match(name: ['is empty']) }
+    end
+
+    context 'when price_in_cents is not integer' do
+      let(:attribute_name) { :price_in_cents }
+
+      it { is_expected.not_to be_valid }
+      it { expect(product_entity.errors).to match(price_in_cents: ['is not integer']) }
+    end
   end
 
   describe '.calculate_in_cents' do
