4fc59e52768504629ff9c9a5aef06fe01cb52fa2
oraul <1838418+oraul@users.noreply.github.com>
2023-07-08 15:33:38 -0300

    Fix typo

--- lib/concerns/loggable.rb
+++ lib/concerns/loggable.rb
@@ -2,8 +2,8 @@
 
 module Loggable
   %i[unknown fatal error warn info debug].each do |log_type|
-    define_method("log_#{log_type}") do |description|
-      LoggerAdapter.public_send(log_type, description)
+    define_method("log_#{log_type}") do |message|
+      LoggerAdapter.public_send(log_type, message)
     end
   end
 end
